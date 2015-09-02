312c9f36f08771a0a5d91b5261de47ba3cb567ab
bpupadhyaya <bpupadhyaya@gmail.com>
2015-09-02 13:03:19 -0700

    Testing changes.

--- src/main/java/com/equalinformation/java/tutorial/HelloWorld.java
+++ src/main/java/com/equalinformation/java/tutorial/HelloWorld.java
@@ -4,6 +4,7 @@ package com.equalinformation.java.tutorial;
  * Created by bpupadhyaya on 9/1/15.
  */
 public class HelloWorld {
+    // TODO Testing repository setup - delete later
     public static void main(String...args) {
         System.out.println("Hello World!");
     }
