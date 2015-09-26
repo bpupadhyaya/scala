41d073b697fb72dca694288cd4a03ef77351a429
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-26 14:23:44 -0700

    P5: More list operations to be added.

--- src/main/scala/com/equalinformation/tutorial/scala/P5_ListOperationsApp.scala
+++ src/main/scala/com/equalinformation/tutorial/scala/P5_ListOperationsApp.scala
@@ -5,20 +5,27 @@ package com.equalinformation.tutorial.scala
  */
 object P5_ListOperationsApp {
   def main(args: Array[String]): Unit = {
+    // Create a new list
     val twoCities = List("Sunnyvale, Stanford")
     val anotherTwoCities = List("Berkeley", "Oakland")
 
-    //List concatenation
+    // List concatenation
     val fourCities = twoCities ::: anotherTwoCities
     println(fourCities)
 
-    //List prepending
+    // List prepending
     val threeCities = "San Jose" :: twoCities
     println(threeCities)
     val fourcitiesPrepending1 = anotherTwoCities :: twoCities
     println(fourcitiesPrepending1)
     val fourCitiesPrepending2 = twoCities :: anotherTwoCities
-    print(fourCitiesPrepending2)
+    println(fourCitiesPrepending2)
+
+    // Create a new list
+    val newList = "this" :: "is" :: Nil
+    println(newList)
+
+    //TODO more list operations here
 
   }
 
