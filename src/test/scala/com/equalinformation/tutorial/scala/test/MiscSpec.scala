7fae596413f7f36bb75373881afbce200e2a6c30
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2015-09-27 17:19:33 -0700

    Assertion style.

--- src/test/scala/com/equalinformation/tutorial/scala/test/MiscSpec.scala
+++ src/test/scala/com/equalinformation/tutorial/scala/test/MiscSpec.scala
@@ -9,18 +9,27 @@ class MiscSpec extends UnitSpec {
   val miscObj = new MiscClassForUnitTesting(4,5)
   val miscObj1 = new MiscClassForUnitTesting(4,0)
 
+  // 1. add method *****************************
   "add" should "produce sum of two numbers" in {
     assert(miscObj.add() == 9)
   }
 
+  // Or
+  assertResult(9) {
+    miscObj.add()
+  }
+
+  // 2. subtract method *****************************
   "subtract" should "produce difference of two numbers" in {
     assert(miscObj.subtract() == -1)
   }
 
+  // 3. multiply method *****************************
   "multiply" should "produce product of two numbers" in {
     assert(miscObj.multiply() == 20)
   }
 
+  // 4. divide method *****************************
   "divide" should "produce quotient of first number divided by second number" in {
     assert(miscObj.divide() == 0)
   }
