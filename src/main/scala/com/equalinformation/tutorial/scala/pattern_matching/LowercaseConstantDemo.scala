ba895f61d24b2aa61635f097a22d22a9a62d6e2b
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2016-02-26 22:37:46 -0800

    Lower case constant pattern matching, remember to use backquotes.

--- src/main/scala/com/equalinformation/tutorial/scala/pattern_matching/LowercaseConstantDemo.scala
+++ src/main/scala/com/equalinformation/tutorial/scala/pattern_matching/LowercaseConstantDemo.scala
@@ -4,5 +4,11 @@ package com.equalinformation.tutorial.scala.pattern_matching
   * Created by bpupadhyaya on 2/26/16.
   */
 object LowercaseConstantDemo extends App {
-  //TODO
+  val someRandomValue = "test"
+  val input = "test"
+
+  input match {
+    case `someRandomValue` => println("someRandomValue 'test' matches with input: "+input)
+    case _ => println("Does not match input: "+input)
+  }
 }
