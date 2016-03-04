566f4018486c005a85014271069178cd20e0efe0
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2016-03-03 20:00:29 -0800

    Selenium testing started.

---
+++ src/test/scala/com/equalinformation/tutorial/scala/test/selenium/BlogSpec.scala
@@ -0,0 +1,23 @@
+package com.equalinformation.tutorial.scala.test.selenium
+
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.scalatest.{ShouldMatchers, FlatSpec}
+
+import org.scalatest.selenium.WebBrowser
+
+/**
+  * Created by bpupadhyaya on 3/3/16.
+  */
+
+class BlogSpec extends FlatSpec with ShouldMatchers with WebBrowser {
+
+  implicit val webDriver: WebDriver = new HtmlUnitDriver
+
+  val host = "http://www.equalinformation.com"
+
+  "The EqualInformation home page" should "have the correct title" in {
+    go to (host + "index.html")
+    pageTitle should be ("")
+  }
+}
